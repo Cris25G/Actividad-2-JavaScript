6339f878cfb99ded1a7a2553b12a41bae8601750
Cristian_Gomez26 <porte20k7@gmail.com>
2022-07-10 17:15:35 -0300

    segundo repo
    mi segundo repositorio JavaScript

---
+++ codigo.js
@@ -0,0 +1,19 @@
+'use strict' // Modo estricto de JavaScript
+
+//Alerta Para El Usuario
+
+alert("Necesitara De Un Internet Optimo Para Cargar Las Imagenes")
+var nombre = "Cristian Carlos Nicolas"
+var apellido = "Gomez"
+alert(nombre+" "+apellido)
+
+//imprimir por consola
+
+console.log("El Formato De La Imagen Debe Ser: width 400px y height auto")
+
+document.write("La Imagen Tiene Formato De Ancho:400px y Alto:auto ")
+
+
+
+
+
